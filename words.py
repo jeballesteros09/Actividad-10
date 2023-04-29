7aa8901d5fd70272285fbccdfb27d01891998b6c
jeballesteros09 <85072859+jeballesteros09@users.noreply.github.com>
2023-04-29 16:11:05 -0500

    Agregando diccionario

---
+++ words.py
@@ -0,0 +1,7 @@
+
+words = [
+    {'palabra': 'Chantin', 'significado': 'Tu casa manito'},
+    {'palabra': 'Manito', 'significado': 'Tu fren'},
+    {'palabra': 'Cuero', 'significado': 'Que procedas'},
+    {'palabra': 'Xutri', 'significado': 'Interjeccion'}
+]
